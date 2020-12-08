bf53ff190f181448b68aa8ff6700aca89b622449
Luis-Eduardo Imbernon <leimbernon@ai-mind.com>
2020-12-08 21:26:07 +0100

    Added background style to the login form

--- screens/LoginComponent.js
+++ screens/LoginComponent.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
-import {View, ScrollView, StyleSheet} from 'react-native';
+import {View, ScrollView, StyleSheet, ImageBackground, Dimensions} from 'react-native';
 import {Input, CheckBox, Button, Icon, Text} from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
-import argonTheme from "../constants/Theme";
+import Images from '../constants/Images';
+import argonTheme from '../constants/Theme';
+
+const { width, height } = Dimensions.get("screen");
 
 class Login extends Component{
 
@@ -34,50 +37,55 @@ class Login extends Component{
 
     render(){
         return(
-            <ScrollView>
-                <View style={styles.container}>
-                    <View style={styles.drawerHeader}>
-                        <Text style={styles.drawerHeaderText}>Login</Text>
-                    </View>
-                    <Input 
-                        placeholder="Server"
-                        leftIcon={{type: 'font-awesome', name: 'server'}}
-                        value={this.props.login.server}
-                        containerStyle={styles.formInput}
-                        onChangeText ={(server) => this.props.onLoginServerChange(server)}
-                    />
-                    <Input 
-                        placeholder="Username"
-                        leftIcon={{type: 'font-awesome', name: 'user-o'}}
-                        value={this.props.login.username}
-                        containerStyle={styles.formInput}
-                        onChangeText = {(username) => this.props.onLoginInfoChange(username, this.props.login.password)}
-                    />
-                    <Input 
-                        placeholder="Password"
-                        secureTextEntry={true}
-                        leftIcon={{type: 'font-awesome', name: 'key'}}
-                        value={this.props.login.password}
-                        containerStyle={styles.formInput}
-                        onChangeText = {(password) => this.props.onLoginInfoChange(this.props.login.username, password)}
-                    />
-                    <CheckBox
-                        title="Remember Me"
-                        center
-                        checked={this.props.login.remember}
-                        containerStyle={styles.formCheckbox}
-                        onPress={() => this.props.onRememberChange(!this.props.login.remember)}
-                    />
-                    <View style={styles.formButton}>
-                        <Button
-                            title='Login'
-                            icon={<Icon name='sign-in' size={24} type='font-awesome' color='white' />}
-                            buttonStyle = {{ backgroundColor: argonTheme.COLORS['PRIMARY'] }}
-                            onPress = {() => this.handleLogin()}
+            <ImageBackground
+                source={Images.RegisterBackground}
+                style={{ width, height, zIndex: 1 }}
+            >
+                <ScrollView>
+                    <View style={styles.container}>
+                        <View style={styles.drawerHeader}>
+                            <Text style={styles.drawerHeaderText}>Login</Text>
+                        </View>
+                        <Input 
+                            placeholder="Server"
+                            leftIcon={{type: 'font-awesome', name: 'server'}}
+                            value={this.props.login.server}
+                            containerStyle={styles.formInput}
+                            onChangeText ={(server) => this.props.onLoginServerChange(server)}
                         />
+                        <Input 
+                            placeholder="Username"
+                            leftIcon={{type: 'font-awesome', name: 'user-o'}}
+                            value={this.props.login.username}
+                            containerStyle={styles.formInput}
+                            onChangeText = {(username) => this.props.onLoginInfoChange(username, this.props.login.password)}
+                        />
+                        <Input 
+                            placeholder="Password"
+                            secureTextEntry={true}
+                            leftIcon={{type: 'font-awesome', name: 'key'}}
+                            value={this.props.login.password}
+                            containerStyle={styles.formInput}
+                            onChangeText = {(password) => this.props.onLoginInfoChange(this.props.login.username, password)}
+                        />
+                        <CheckBox
+                            title="Remember Me"
+                            center
+                            checked={this.props.login.remember}
+                            containerStyle={styles.formCheckbox}
+                            onPress={() => this.props.onRememberChange(!this.props.login.remember)}
+                        />
+                        <View style={styles.formButton}>
+                            <Button
+                                title='Login'
+                                icon={<Icon name='sign-in' size={24} type='font-awesome' color='white' />}
+                                buttonStyle = {{ backgroundColor: argonTheme.COLORS['PRIMARY'] }}
+                                onPress = {() => this.handleLogin()}
+                            />
+                        </View>
                     </View>
-                </View>
-            </ScrollView>
+                </ScrollView>
+            </ImageBackground>
         );
     }
 }
